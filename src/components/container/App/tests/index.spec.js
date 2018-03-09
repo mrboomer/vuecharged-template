1727e30863d10da9c5ece2f4548561b336499b1a
Daniel Escobedo <daniel@adquadrant.com>
2018-03-08 17:00:51 -0800

    Created Tests

--- src/components/container/App/tests/index.spec.js
+++ src/components/container/App/tests/index.spec.js
@@ -1,11 +1,11 @@
 import { shallow } from '@vue/test-utils';
-import HomeComponent from '../Home';
+import AppComponent from '../index';
 
 const renderer = require('vue-server-renderer').createRenderer();
 
-describe('Home.vue Component', () => {
-  const wrapper = shallow(HomeComponent);
-  const { vm } = wrapper;
+describe('App Component', () => {
+  const ClonedComponent = shallow(AppComponent);
+  const { vm } = ClonedComponent;
 
   it('renders to a nice snapshot', () => {
     renderer.renderToString(vm, (err, str) => {
